5c3ab287e05f025c8857e73e6d39bf5a82c1e48a
Alexandre Ignjatovic <alexandre.ignjatovic@gmail.com>
2013-10-24 12:47:49 +0200

    Addition of the Gene class and subsequent inheriting classes (IntegerGene, FloatGene and EnumGene)

---
+++ lib/gene.rb
@@ -0,0 +1,69 @@
+
+class Gene  
+  def mutate(allele)
+    raise "the 'mutate' function must be overloaded in the inheriting class."
+  end
+
+  def random_allele_value
+    raise "the 'random_allele_value' function must be overloaded in the inheriting class."
+  end
+
+  def create_random
+    Allele.new(self, self.random_allele_value)
+  end
+end
+
+class NumericGene < Gene
+  attr_accessor :min, :max
+  def initialize(min, max)
+    raise "max (#{max}) must be greater than min (#{min})" if max <= min
+    @min = min
+    @max = max
+  end
+
+  def mutate(previous_value) self.random end
+end 
+
+class IntegerGene < NumericGene
+  def initialize(min, max) super(min, max) end
+  
+  def random_allele_value
+    rand(max - self.min + 1) + self.min
+  end
+end
+
+class FloatGene < NumericGene
+  def initialize(min, max) super(min, max) end
+  
+  def random_allele_value
+    rand() * (self.max - self.min) + self.min
+  end
+end
+
+class EnumGene < Gene
+  attr_accessor :enum_values
+
+  def initialize(enum_values)
+    raise "EnumGene initialization require an Array" unless enum_values.is_a? Array
+    raise "EnumGene require at least two values" unless enum_values.size > 1
+    @enum_values = enum_values
+  end
+  
+  def random_allele_value
+    @enum_values[rand @enum_values.size]
+  end
+  
+  def mutate(previous_value)
+    new_value = self.random_allele_value
+    while new_value == previous_value
+      new_value = self.random_allele_value
+    end
+    new_value
+  end
+end
+
+class Gene
+  def self.Integer(min, max) IntegerGene.new(min, max) end
+  def self.Float(min, max) FloatGene.new(min, max) end
+  def self.Enum(enum_values) EnumGene.new(enum_values) end
+end
---
+++ spec/gene_spec.rb
@@ -0,0 +1,32 @@
+require 'gene'
+
+describe Gene do
+
+  before(:each) do
+  end
+  
+  it "create an integer allele with a value" do
+    gene = Gene.Integer(-20, 20)
+    allele = gene.create_random
+    expect(allele.value <= 20)
+    expect(allele.value >= -20)
+  end
+
+  it "create a float allele with a value" do
+    gene = Gene.Integer(-2.0, 2.0)
+    allele = gene.create_random
+    expect(allele.value <= 2.0)
+    expect(allele.value >= -2.0)
+  end
+
+  it "create an allele from an enum" do
+    enum_values_array = [1, 2, 3]
+    gene = Gene.Enum(enum_values_array)
+    allele = gene.create_random
+    expect(enum_values_array).to include(allele.value)
+    previous_value = allele.value
+    allele.mutate
+    expect(previous_value != allele.value).to be_true
+  end
+
+end
