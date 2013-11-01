c2a347f8eef0b4f90405163a191ac9d3ae9a46fe
Alexandre Ignjatovic <alexandre.ignjatovic@gmail.com>
2013-11-01 19:46:01 +0100

    rdoc comments added for the karyotype.rb file

--- lib/karyotype.rb
+++ lib/karyotype.rb
@@ -1,21 +1,30 @@
 require 'chromosome'
 require 'digest/md5'
+# This class represent a karyotype, which could be seen as a set of
+# chromosomes representing a specific individual of a population
 class Karyotype
   attr_accessor :chromosomes
   attr_accessor :fitness
+  
   def initialize(genome, chromosomes_description)
     @genome = genome
     @chromosomes_description = chromosomes_description
   end
   private :initialize
+  
+  # Copy self and all its chromosomes to a new karyotype
   def copy
     karyotype = Karyotype.new(@genome, @chromosomes_description)
     karyotype.chromosomes = self.chromosomes.map{|chromosome| chromosome.copy}
     karyotype
   end
+  
   def to_s
     @genome.gene_positions.keys.map{|gn| "#{gn}=>#{self[gn]}"}.join';'
   end
+  
+  # Create a random karyotype from a specific genome an its associated
+  # chromosomes description
   def Karyotype.create_random_from(genome, chromosomes_description)
     karyotype = Karyotype.new(genome, chromosomes_description)
     karyotype.chromosomes = chromosomes_description.map {|description|
@@ -24,6 +33,8 @@ class Karyotype
     karyotype
   end
   
+  # Return the allele value of a specific named gene
+  # We strongly recommand using symbols as gene name
   def [](gene_name)
     return nil if @chromosomes.nil?
     chromosome_position, gene_position = @genome.get_gene_position(gene_name)
@@ -31,6 +42,8 @@ class Karyotype
     return @chromosomes[chromosome_position][gene_position].value
   end
   
+  # Breeding function
+  # Create a new karyotype based on self and an other
   def +(other)
     child = copy
     other.chromosomes.each_with_index {
@@ -39,6 +52,7 @@ class Karyotype
     child
   end
   
+  # Aggregate all the allele into a md5 hash value
   def to_md5()
     if (@hash_value.nil?) then
       @hash_value = @chromosomes.map{ |chromosome|
@@ -49,6 +63,7 @@ class Karyotype
     @hash_value
   end
   
+  # Randomly mutate an allele of a randomly selected chromosome
   def mutate
     @chromosomes[rand @chromosomes.size].mutate
     self
